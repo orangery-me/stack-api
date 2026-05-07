828979d13a8aed92c64b098085518a29a511f81e
orangery-me <102220086@sv1.dut.udn.vn>
2026-05-07 17:06:19 +0700

    update origin CORS

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
   const configService = app.get(ConfigService);
 
   app.enableCors({
-    origin: configService.get<string>('CORS_ORIGIN'),
+    origin: ['https://stack-fe.vercel.app', 'http://localhost:5173'],
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
